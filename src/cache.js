ed17c7c966bc28662e0cf55bc1ac567654ebcb8d
Azury <azury.wtf@gmail.com>
2021-09-28 20:58:07 +0200

    enhanced overall cache

--- src/cache.js
+++ src/cache.js
@@ -1,5 +1,5 @@
 /*!
- * Devyl's Cache v1.0.2
+ * Devyl's Cache v1.0.3
  * © 2021 - Samuel Kopp
  * Code licensed under Apache-2.0
  */
@@ -7,12 +7,32 @@
 export default class cache {
   // contructor
   constructor(config) {
+    this.max = config.max || Infinity
     this.maxAge = config.maxAge || 900
-    this.maxLength = config.length || 100
+    this.maxLength = config.length || Infinity
     this.store = []
   }
 
   setKey = async (key, value) => {
+    // make sure key and value are valid
+    if (!key || !value) return false
+
+    // make sure key is unique
+    const index = this.store.findIndex(i => i.key === key)
+
+    // item doesn't exist in store
+    if (index !== -1) return false
+
+    // if key is a string and longer than the maximum length
+    if (typeof value === 'string' && value.length > this.maxLength) return false
+
+    // if cache has already hit the maximum amount of items, delete the least-recently used one
+    if (this.store.length === this.max) {
+      const max = await this.store.reduce((prev, current) => (prev.maxAge < current.maxAge) ? prev : current)
+      const i = this.store.findIndex(i => i.key === max.key)
+      this.store.splice(i, 1)
+    }
+
     // create item
     const item = {
       key: key,
@@ -20,11 +40,20 @@ export default class cache {
       createdAt: Date.now()
     }
 
+    // delete all items exceeding max age except this one
+    this.store = this.store.filter(i => (Date.now() - i.createdAt < this.maxAge * 1000) && (i.key !== key))
+
     // push item to store
     this.store.push(item)
+
+    // let user know everything went alright
+    return true
   }
 
   getKey = async (key) => {
+    // make sure key and value are valid
+    if (!key) return null
+
     // search for item
     const index = this.store.findIndex(i => i.key === key)
 
@@ -35,21 +64,24 @@ export default class cache {
     const item = this.store[index]
 
     // delete all items exceeding max age except this one
-    this.store = this.store.filter(i => (Date.now() - i.createdAt < this.maxAge * 1000) && (i.key !== key))
+    this.store = this.store.filter(i => (Date.now() - i.createdAt < this.maxAge * 1000) || (i.key === key))
 
     // update max age of item
-    this.store[index].createdAt = Date.now()
+    item.createdAt = Date.now()
 
     // return value
     return item.value
   }
 
   updateKey = async (key, value) => {
+    // make sure key and value are valid
+    if (!key || !value) return false
+
     // search for item
     const index = this.store.findIndex(i => i.key === key)
     
     // item doesn't exist in store
-    if (index === -1) return null
+    if (index === -1) return false
 
     // update item
     this.store[index] = {
@@ -58,11 +90,14 @@ export default class cache {
       createdAt: Date.now()
     }
 
-    // return value of updated item
-    return value
+    // let user know everything went alright
+    return true
   }
 
   stealKey = async (key) => {
+    // make sure key and value are valid
+    if (!key) return null
+
     // search for item
     const index = this.store.findIndex(i => i.key === key)
 
@@ -72,11 +107,17 @@ export default class cache {
     // get item
     const item = this.store[index]
 
+    // delete all items exceeding max age except this one
+    this.store = this.store.filter(i => (Date.now() - i.createdAt < this.maxAge * 1000) || (i.key === key))
+
     // return value
     return item.value
   }
 
   hasKey = async (key) => {
+    // make sure key and value are valid
+    if (!key) return false
+
     // search for item
     const index = this.store.findIndex(i => i.key === key)
 
@@ -88,6 +129,9 @@ export default class cache {
   }
 
   hasValue = async (value) => {
+    // make sure key and value are valid
+    if (!value) return false
+
     // search for item
     const index = this.store.findIndex(item => item.value === value)
 
@@ -99,20 +143,27 @@ export default class cache {
   }
 
   deleteKey = async (key) => {
+    // make sure key and value are valid
+    if (!key) return false
+
     // search for item
     const index = this.store.findIndex(i => i.key === key)
 
     // item doesn't exist in store
-    if (index === -1) return null
+    if (index === -1) return false
 
     // remove item from store
     this.store.splice(index, 1)
 
+    // let user know everything went alright
     return true
   }
 
   purge = async () => {
     // reset store
     store = []
+
+    // let user know everything went alright
+    return true
   }
 }
