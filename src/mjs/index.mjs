259f359c380a328b749f041a6439b3b3c37f897a
Azury <azury.wtf@gmail.com>
2021-10-20 18:52:09 +0200

    added mjs modules

---
+++ src/mjs/index.mjs
@@ -0,0 +1,4 @@
+import instance from './instance.js'
+export default instance
+
+export { global, setItem, getItem, updateItem, viewItem, hasItem, deleteItem, prune, purge, getItems, getAmountOfItems, getItemsByCondition, purgeItemsByCondition, getValuesOfItems, getKeysOfItems } from './global.js'
