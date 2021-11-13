30e8db8d411d4761913c63057e010f878740afa2
Azury <azury.wtf@gmail.com>
2021-11-13 19:33:20 +0100

    added jsdoc declarations

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-interface Config {
+import fs from 'fs'
+
+interface Configuration {
   maxAge?: number | undefined;
   maxAmount?: number | undefined;
 }
@@ -8,7 +10,13 @@ export class Cachu {
   private maxAge: Number
   private store: Array<any>
 
-  constructor(config?: Config) {
+  /**
+   * **Create New Cache Instance**
+   * 
+   * Create a new instance of cachu.
+   * @param config
+   */
+  constructor(config?: Configuration) {
     // check config
     if (!config) config = {}
 
@@ -18,6 +26,13 @@ export class Cachu {
     this.store = []
   }
 
+  /**
+   * **Add New Item**
+   * 
+   * Add a new item to the cache.
+   * @param key
+   * @param value
+   */
   set = async (key: {}, value: {}) => {
     // validate uniqueness of key
     const index = this.store.findIndex(i => i[0] === key)
@@ -37,7 +52,13 @@ export class Cachu {
   
     return true
   }
-  
+
+  /**
+   * **Get an Item**
+   * 
+   * Get the content of an existing item.
+   * @param key
+   */
   get = async (key: {}) => {
     // remove outdated items
     this.store = this.store.filter(i => Date.now() - i[2] < this.maxAge.valueOf() * 1000)
@@ -52,7 +73,13 @@ export class Cachu {
     // return value of item
     return item[1]
   }
-  
+
+  /**
+   * **View a Item**
+   * 
+   * Get the content of an existing item without removing any overaged items or without returning `null` if it's overaged.
+   * @param key
+   */
   view = async (key: {}) => {
     // remove outdated items
     this.store = this.store.filter(i => (Date.now() - i[2] < this.maxAge.valueOf() * 1000) || (i[0] === key))
@@ -68,6 +95,13 @@ export class Cachu {
     return item[1]
   }
   
+  /**
+   * **Update a Item**
+   * 
+   * Update the content of an existing item. 
+   * @param key
+   * @param value
+   */
   update = async (key: {}, value: {}) => {
     // search for item
     const index = this.store.findIndex(i => i[0] === key)
@@ -79,6 +113,12 @@ export class Cachu {
     return true
   }
   
+  /**
+   * **Has a Item**
+   * 
+   * Check if the cache has an item with the specified key. 
+   * @param key
+   */
   has = async (key: {}) => {
     // search for item
     const index = this.store.findIndex(i => i[0] === key)
@@ -87,6 +127,12 @@ export class Cachu {
     return true
   }
   
+  /**
+   * **Purge a Item**
+   * 
+   * Delete the item with the specified key.
+   * @param key
+   */
   purge = async (key: {}) => {
     // search for item
     const index = this.store.findIndex(i => i[0] === key)
@@ -98,13 +144,23 @@ export class Cachu {
     return true
   }
   
+  /**
+   * **Prune Cache**
+   * 
+   * Delete all overaged items manually.
+   */
   prune = async () => {
-    // remove outdated items
+    // remove overaged items
     if (this.store.length > 0) this.store = this.store.filter(i => Date.now() - i[2] < this.maxAge.valueOf() * 1000)
   
     return true
   }
   
+  /**
+   * **Destroy Cache**
+   * 
+   * Delete all items.
+   */
   destroy = async () => {
     // reset this.store
     this.store = []
@@ -112,6 +168,12 @@ export class Cachu {
     return true
   }
   
+  /**
+   * **Purge Many Items**
+   * 
+   * Delete multiple items by their keys.
+   * @param keys
+   */
   purgeMany = async (keys: Array<any>) => {
     // purge each item
     for (const key of keys) 
@@ -120,6 +182,12 @@ export class Cachu {
     return true
   }
   
+  /**
+   * **Get Many Items**
+   * 
+   * Get multiple items by their keys.
+   * @param keys
+   */
   getMany = async (keys: Array<any>) => {
     // create the array
     const items = []
@@ -129,38 +197,81 @@ export class Cachu {
       const item = await this.get(key)
       if (item !== null || item !== undefined) items.push(item)
     }
-  
+
+    // return items (without age)
+    // return items.map((item: Array<any>) => item.splice(2, 1))
     return items
   }
   
+  /**
+   * **Purge Many Items by a Condition**
+   * 
+   * Delete multiple items by a specific condition.
+   * @param condition
+   */
   purgeManyByCondition = async (condition: Function) => {
     // purge each item
-    this.store = this.store.filter(item => !condition(item))
+    this.store = this.store.filter(i => !condition(i))
   
     return true
   }
   
+  /**
+   * **Get Multiple Items by a Condition**
+   * 
+   * Delete multiple items by a specific condition.
+   * @param condition
+   */
   getManyByCondition = async (condition: Function) => {
-    // return items
-    return this.store.filter(item => condition(item))
+    // filter items
+    const filteredItems = this.store.filter(i => condition(i))
+
+    const itemValues: Array<any> = []
+    for (const item of filteredItems) {
+      itemValues.push(item[1])
+    }
+
+    // return item values
+    return itemValues
   }
   
+  /**
+   * **Get Amount of Items**
+   * 
+   * Get the amount of items the cache has.
+   */
   getAmountOfItems = async () => {
     return this.store.length
   }
   
+  /**
+   * **Get Values of Items**
+   * 
+   * Get an array of all the item values.
+   */
   getValuesOfItems = async () => {
     if (this.store.length === 0) return
   
     return this.store.map(item => item[1])
   }
   
+  /**
+   * **Get Keys of Items**
+   * 
+   * Get an array of all item keys.
+   */
   getKeysOfItems = async () => {
     if (this.store.length === 0) return
-  
+
     return this.store.map(item => item[0])
   }
   
+  /**
+   * **Execute an Action on Each Item**
+   * 
+   * Executes a specific function on each item.
+   * @param action
+   */
   each = async (action: Function) => {
     // execute action on each item
     this.store.forEach(async item => {
