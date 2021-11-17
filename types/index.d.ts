7933f7c4b8bfbed274954a7375a7c69515712c83
Azury <azury.wtf@gmail.com>
2021-11-17 18:43:36 +0100

    updated types

--- types/index.d.ts
+++ types/index.d.ts
@@ -22,21 +22,21 @@ export declare class Cachu {
      */
     set: (key: {}, value: {}) => Promise<boolean>;
     /**
-     * **Get an Item**
+     * **Get Item**
      *
      * Get the content of an existing item.
      * @param key
      */
     get: (key: {}) => Promise<any>;
     /**
-     * **View a Item**
+     * **View Item**
      *
      * Get the content of an existing item without removing any overaged items or without returning `null` if it's overaged.
      * @param key
      */
     view: (key: {}) => Promise<any>;
     /**
-     * **Update a Item**
+     * **Update Item**
      *
      * Update the content of an existing item.
      * @param key
@@ -44,14 +44,14 @@ export declare class Cachu {
      */
     update: (key: {}, value: {}) => Promise<boolean>;
     /**
-     * **Has a Item**
+     * **Has Item**
      *
      * Check if the cache has an item with the specified key.
      * @param key
      */
     has: (key: {}) => Promise<boolean>;
     /**
-     * **Purge a Item**
+     * **Purge Item**
      *
      * Delete the item with the specified key.
      * @param key
@@ -70,53 +70,53 @@ export declare class Cachu {
      */
     destroy: () => Promise<boolean>;
     /**
-     * **Purge Many Items**
+     * **Purge Multiple Items**
      *
      * Delete multiple items by their keys.
      * @param keys
      */
     purgeMany: (keys: Array<any>) => Promise<boolean>;
     /**
-     * **Get Many Items**
+     * **Get Multiple Items**
      *
      * Get multiple items by their keys.
      * @param keys
      */
     getMany: (keys: Array<any>) => Promise<any[]>;
     /**
-     * **Purge Many Items by a Condition**
+     * **Purge Multiple Items Conditionally**
      *
      * Delete multiple items by a specific condition.
      * @param condition
      */
     purgeManyByCondition: (condition: Function) => Promise<boolean>;
     /**
-     * **Get Multiple Items by a Condition**
+     * **Get Multiple Items Conditionally**
      *
      * Delete multiple items by a specific condition.
      * @param condition
      */
     getManyByCondition: (condition: Function) => Promise<any[]>;
     /**
-     * **Get Amount of Items**
+     * **Get Amount Of Items**
      *
      * Get the amount of items the cache has.
      */
     getAmountOfItems: () => Promise<number>;
     /**
-     * **Get Values of Items**
+     * **Get Values Of Items**
      *
      * Get an array of all the item values.
      */
     getValuesOfItems: () => Promise<any[] | undefined>;
     /**
-     * **Get Keys of Items**
+     * **Get Keys Of Items**
      *
      * Get an array of all item keys.
      */
     getKeysOfItems: () => Promise<any[] | undefined>;
     /**
-     * **Execute an Action on Each Item**
+     * **Iterate Over Items**
      *
      * Executes a specific function on each item.
      * @param action
