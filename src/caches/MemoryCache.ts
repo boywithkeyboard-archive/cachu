ca12a0c18bfc4b00dd6edb0f998e2609087f1a11
Azury <azury.wtf@gmail.com>
2022-01-24 18:50:33 +0100

    edited `MemoryCache`

--- src/caches/MemoryCache.ts
+++ src/caches/MemoryCache.ts
@@ -1,15 +1,38 @@
-import os from 'os'
+import { Hooks, Key, Value, Entry, KeyValue } from '../types'
+
+interface Configuration {
+  /**
+   * Customize **cachu**'s functionality to your desire.
+   */
+  hooks?: Hooks,
+
+  /**
+   * Set the maximum age for cache entries.
+   */
+  maxAge?: number,
+
+  /**
+   * Set the maximum amount of entries the cache can hold.
+   */
+  maxAmount?: number,
+
+  /**
+   * Allow overriding of entries on writing.
+   */
+  overrideEntries?: boolean
+}
 
 export default class MemoryCache {
   private maxAge: number
   private maxAmount: number
-  private maxMemory: number
   private overrideEntries: boolean
   private hooks: Hooks
   private store: Map<Key, { value: Value, createdAt: number }>
 
   /**
    * Create a new `MemoryCache`.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/caches/MemoryCache.md)
    */
   constructor(config?: Configuration) {
     if (!config) config = {}
@@ -22,9 +45,6 @@ export default class MemoryCache {
 
     // if entry exists already, override existing one on writing
     this.overrideEntries = (config.overrideEntries === true)
-
-    // maximum memory in megabytes
-    this.maxMemory = config.maxMemory ? config.maxMemory * 1024 * 1024 :  os.freemem() * 0.8
 
     // hooks
     this.hooks = config.hooks ?? {}
@@ -55,6 +75,8 @@ export default class MemoryCache {
 
   /**
    * Create a new entry in the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/write.md)
    */
   write = async (key: Key, value: Value) => {
     // create new entry
@@ -67,27 +89,26 @@ export default class MemoryCache {
     // trigger hook
     if (this.hooks?.preWriting) {
       newEntry = await this.hooks.preWriting({ entry: newEntry })
-      if (!newEntry) return false
+      if (!newEntry) return
     }
 
     // check uniqueness of key
-    if (this.store.has(key) && !this.overrideEntries) return false
+    if (this.store.has(key) && !this.overrideEntries) return
   
     // remove outdated entries
     await this.purgeOutdatedEntries()
   
-    // remove oldest entry if store has too much entries or the cache exceeds its maximum memory
-    if (this.store.size === this.maxAmount || os.freemem() < this.maxMemory) await this.purgeOldestEntry()
+    // remove oldest entry if store has too much entries
+    if (this.store.size === this.maxAmount) await this.purgeOldestEntry()
   
     // add new entry to store
     this.store.set(key, { value: newEntry.value, createdAt: newEntry.createdAt })
-
-    // let the user know everything went alright
-    return true
   }
 
   /**
-   * Create a new entry in the cache.
+   * Create many new entries in the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/writeMany.md)
    */
   writeMany = async (entries: KeyValue[]) => {
     for (const entry of entries) {
@@ -101,28 +122,290 @@ export default class MemoryCache {
       // trigger hook
       if (this.hooks?.preWriting) {
         newEntry = await this.hooks.preWriting({ entry: newEntry })
-        if (!newEntry) return false
+        if (!newEntry) continue
       }
 
       // check uniqueness of key
-      if (this.store.has(entry.key) && !this.overrideEntries) return false
+      if (this.store.has(entry.key) && !this.overrideEntries) continue
   
       // remove outdated entries
       await this.purgeOutdatedEntries()
   
-      // remove oldest entry if store has too much entries or the cache exceeds its maximum memory
-      if (this.store.size === this.maxAmount || os.freemem() < this.maxMemory) await this.purgeOldestEntry()
+      // remove oldest entry if store has too much entries
+      if (this.store.size === this.maxAmount) await this.purgeOldestEntry()
   
       // add new entry to store
       this.store.set(entry.key, { value: newEntry.value, createdAt: newEntry.createdAt })
     }
-
-    // let the user know everything went alright
-    return true
   }
 
   /**
-   * Calculates the amount of memory consumed by the cache.
+   * Read an entry from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/get.md)
    */
-  getConsumedMemory = async () => {}
+  get = async (key: Key) => {
+    if (this.hooks?.preReading && !(await this.hooks.preReading({ keyOfTargetedEntry: key }))) return null
+
+    await this.purgeOutdatedEntries()
+
+    const entry = this.store.get(key)
+
+    if (entry) return entry.value
+    return null
+  }
+
+  /**
+   * Read many entries from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/getMany.md)
+   */
+  getMany = async (keys: Key[]) => {
+    await this.purgeOutdatedEntries()
+
+    const data = []
+
+    for (const key of keys) {
+      if (this.hooks?.preReading && !(await this.hooks.preReading({ keyOfTargetedEntry: key }))) {
+        data.push(null)
+      } else {
+        let entry = this.store.get(key)
+
+        if (entry) data.push(entry.value)
+        else data.push(null)
+      }
+    }
+
+    return data
+  }
+
+  /**
+   * Grab an entry from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/grab.md)
+   */
+  grab = async (key: Key) => {
+    if (this.hooks?.preGrabbing && !(await this.hooks.preGrabbing({ keyOfTargetedEntry: key }))) return null
+
+    const entry = this.store.get(key)
+  
+    if (entry) return entry.value
+    return null
+  }
+
+  /**
+   * Grab many entries from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/grabMany.md)
+   */
+  grabMany = async (keys: Key[]) => {
+    const data = []
+  
+    for (const key of keys) {
+      if (this.hooks?.preGrabbing && !(await this.hooks.preGrabbing({ keyOfTargetedEntry: key }))) {
+        data.push(null)
+      } else {
+        let entry = this.store.get(key)
+  
+        if (entry) data.push(entry.value)
+        else data.push(null)
+      }
+    }
+  
+    return data
+  }
+
+  /**
+   * Steal an entry from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/steal.md)
+   */
+  steal = async (key: Key) => {
+    if (this.hooks?.preStealing && !(await this.hooks.preStealing({ keyOfTargetedEntry: key }))) return null
+
+    await this.purgeOutdatedEntries()
+
+    // get entry from cache
+    const entry = this.store.get(key)
+
+    // make sure entry exists
+    if (!entry) return null
+
+    // remove entry from cache
+    this.store.delete(key)
+  
+    return entry.value
+  }
+
+  /**
+   * Steal many entries from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/stealMany.md)
+   */
+  stealMany = async (keys: Key[]) => {
+    await this.purgeOutdatedEntries()
+
+    const data = []
+
+    for (const key of keys) {
+      if (this.hooks?.preStealing && !(await this.hooks.preStealing({ keyOfTargetedEntry: key }))) {
+        data.push(null)
+      } else {
+        // get entry from cache
+        const entry = this.store.get(key)
+
+        if (entry) {
+          data.push(entry.value)
+
+          // remove entry from cache
+          this.store.delete(key)
+        } else {
+          data.push(null)
+        }
+      }
+    }
+
+    return data
+  }
+
+  /**
+   * Modify an entry in the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/update.md)
+   */
+  update = async (key: Key, value: Value) => { 
+    if (!this.store.has(key)) return
+
+    if (this.hooks?.preUpdating && !(await this.hooks.preUpdating({ keyOfTargetedEntry: key }))) return
+
+    this.store.set(key, {
+      value: value,
+      createdAt: Date.now()
+    })
+  }
+
+  /**
+   * Modify many entries in the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/updateMany.md)
+   */
+  updateMany = async (entries: KeyValue[]) => {
+    for (const entry of entries) {
+      if (!this.store.has(entry.key)) continue
+
+      if (this.hooks?.preUpdating && !(await this.hooks.preUpdating({ keyOfTargetedEntry: entry.key }))) continue
+  
+      this.store.set(entry.key, {
+        value: entry.value,
+        createdAt: Date.now()
+      })
+    }
+  }
+
+  /**
+   * Purge an entry from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/purge.md)
+   */
+  purge = async (key: Key) => {
+    if (this.hooks?.prePurging && !(await this.hooks.prePurging({ keyOfTargetedEntry: key }))) return
+    
+    this.store.delete(key)
+  }
+
+  /**
+   * Purge many or all entries from the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/purgeMany.md)
+   */
+  purgeMany = async (keys: Key[]) => {
+    if (keys.length === 0) {
+      // purge all entries from the cache
+      this.store.clear()
+    } else {
+      // purge many entries from the cache
+      for (const key of keys) {
+        if (this.hooks?.prePurging && !(await this.hooks.prePurging({ keyOfTargetedEntry: key }))) continue
+        this.store.delete(key)
+      }
+    }
+  }
+
+  /**
+   * Check whether the cache has a specific entry.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/has.md)
+   */
+  has = async (key: Key) => {
+    return this.store.has(key)
+  }
+
+  /**
+   * Get the number of entries the cache holds.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/getAmountOfEntries.md)
+   */
+  getAmountOfEntries = async () => {
+    return this.store.size
+  }
+
+  /**
+   * Get an array of all entry keys.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/getKeysOfEntries.md)
+   */
+  getKeysOfEntries = async () => {
+    return [...this.store.keys()].reverse()
+  }
+
+  /**
+   * Get an array of all entry values.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/getValuesOfEntries.md)
+   */
+  getValuesOfEntries = async () => {
+    // get values from cache
+    const values = [...this.store.values()]
+    const structuredValues = []
+
+    // return only the values, not the createdAt property
+    for(const value of values) structuredValues.push(value.value)
+
+    return structuredValues.reverse()
+  }
+
+  /**
+   * Purge all stale entries manually.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/prune.md)
+   */
+  prune = async () => {
+    if (this.hooks?.prePruning) {
+      const entries = [...this.store.entries()]
+      const structuredEntries: Entry[] = []
+      
+      for (const entry of entries) {
+        structuredEntries.push({
+          key: entry[0],
+          value: entry[1].value,
+          createdAt: entry[1].createdAt
+        })
+      }
+
+      await this.hooks.prePruning({ entries: structuredEntries })
+    }
+
+    await this.purgeOutdatedEntries()
+  }
+
+  /**
+   * Calculates the amount of memory in bytes consumed by the cache.
+   * 
+   * [📖 Read the Guide](https://github.com/azurydev/cachu/blob/current/guide/features/getConsumedMemory.md)
+   */
+  getConsumedMemory = async () => {
+    const values = [...this.store.values()].toString()
+    const buffer = Buffer.from(values)
+    return Buffer.byteLength(buffer)
+  }
 }
