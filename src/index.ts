5928490d1923676806fa3e1c3732bbf0e44fcd0e
Azury <azury.wtf@gmail.com>
2022-04-07 19:24:27 +0200

    edited main node script

--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as MemoryCache } from './caches/MemoryCache'
-export { default as BrowserCache } from './caches/BrowserCache'
-export { default as DiskCache } from './caches/DiskCache'
-export { default as FileCache } from './caches/FileCache'
---
+++ src/node.ts
@@ -0,0 +1,3 @@
+export { default as MemoryCache } from './caches/MemoryCache'
+export { default as DiskCache } from './caches/DiskCache'
+export { default as RedisCache } from './caches/RedisCache'
