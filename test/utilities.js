f7fc576525290955c0e13d8af9974147dc6c21a9
Azury <azury.wtf@gmail.com>
2022-04-10 17:38:19 +0200

    removed old tests

--- test/utilities.js
+++
@@ -1,7 +0,0 @@
-export const generateEntries = async (cache, amount) => {
-  let iterations = amount
-  while(iterations > 0) {
-    await cache.write(iterations, `smth${iterations}`)
-    iterations--
-  }
-}
