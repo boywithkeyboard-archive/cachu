ecebc6fd57374803e009dec90fa758a0947deeea
Azury <azury.wtf@gmail.com>
2022-04-10 17:37:56 +0200

    edited node version

--- src/node.ts
+++ src/node.ts
@@ -1,3 +1,2 @@
 export { default as MemoryCache } from './caches/MemoryCache'
-export { default as DiskCache } from './caches/DiskCache'
-export { default as RedisCache } from './caches/RedisCache'
+// export { default as RedisCache } from './caches/RedisCache'
